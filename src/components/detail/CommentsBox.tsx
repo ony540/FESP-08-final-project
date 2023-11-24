244f2856a4df29cd2d13ec7f01a80f927d5ff547
NAYEONG OH <102240503+ony540@users.noreply.github.com>
2023-11-24 10:37:59 +0900

    ✨ feat: 댓글 CRD 기능 구현 #43

--- src/components/detail/CommentsBox.tsx
+++ src/components/detail/CommentsBox.tsx
@@ -1,8 +1,9 @@
-import { getComments } from '@API'
+import { deleteComment, getComments, uploadComment } from '@API'
+import { CommentType } from '@types'
 import { useEffect, useState } from 'react'
 
 export const CommentsBox = ({ videoId }: { videoId: string }) => {
-  const [comments, setComments] = useState()
+  const [comments, setComments] = useState<CommentType[]>([])
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,5 +16,58 @@ export const CommentsBox = ({ videoId }: { videoId: string }) => {
   useEffect(() => {
     console.log(comments)
   }, [comments])
-  return <div></div>
+
+  const [comment, setComment] = useState<string>('')
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setComment(e.target.value)
+  }
+
+  const handleUploadForm = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const uploadedData = (await uploadComment({
+      commentInput: comment,
+      videoId
+    })) as CommentType[]
+    setComment('')
+    setComments(prevData => [uploadedData[0], ...prevData])
+  }
+
+  const handleClickDelete = async (id: number) => {
+    await deleteComment(id)
+    setComments(comments.filter(comment => comment.id !== id))
+  }
+
+  return (
+    <>
+      <form onSubmit={handleUploadForm}>
+        <input
+          id="commentInput"
+          type="text"
+          placeholder="댓글을 달아주세요"
+          value={comment}
+          onChange={handleInputChange}
+        />
+        <label
+          htmlFor="commentInput"
+          className="a11y-hidden">
+          댓글 달기
+        </label>
+        <button type="submit">작성</button>
+      </form>
+
+      <ul>
+        {comments?.map(comment => (
+          <li key={comment.id}>
+            <p>{comment.text}</p>
+            <button
+              type="button"
+              onClick={() => handleClickDelete(comment.id)}>
+              x
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
